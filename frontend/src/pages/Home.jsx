9148d14c6efbd9e7654fe98bac28c3cd009e6ec3
Paula Oliveira <oliveirapaulaa@gmail.com>
2024-04-08 19:59:18 -0300

    Add home, login and register ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom"
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa"
+
 export default function Home() {
-  return (
-    <div>Home</div>
-  )
+  return <>
+    <section className="heading">
+        <h1>What do you need help with?</h1>
+        <p>Please choose from an option below</p>
+    </section>
+    <Link to='/new-ticket' className="btn btn-reverse btn-block">
+        <FaQuestionCircle/> Create new ticket
+    </Link>
+    <Link to='/tickets' className="btn btn-block">
+        <FaTicketAlt/> View my tickets
+    </Link>
+  </>
 }
