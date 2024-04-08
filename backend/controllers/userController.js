d85b52716c6b5d148f948511664948f1b5d02bbc
Paula Oliveira <oliveirapaulaa@gmail.com>
2024-04-08 15:50:29 -0300

    Add user register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/userModel')
 
 // @desc  Register a new user
 // @route /api/users
@@ -11,7 +14,35 @@ const registerUser = asyncHandler(async(req, res) => {
         throw new Error('Please include all fields')
     }
 
-    res.send('Register Route')
+    // Find if user already exists
+    const userExist = await User.findOne({ email })
+
+    if(userExist) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // Has password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //Create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new errpr('Invalid user data')
+    }
 }) 
 
 // @desc  Login a new user
