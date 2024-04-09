289723ddcf2984e53e2e3c826bdba7d2179e29fb
Paula Oliveira <oliveirapaulaa@gmail.com>
2024-04-09 17:28:04 -0300

    Add logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,19 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
 
 export default function Header() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { user } = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
   return (
     <header className="header">
         <div className='logo'>
@@ -11,16 +23,30 @@ export default function Header() {
         </div>
 
         <ul>
-            <li>
-                <Link to='/login'>
-                    <FaSignInAlt /> Login
-                </Link>
-            </li>
-            <li>
-                <Link to='/register'>
-                    <FaUser /> Register
-                </Link>
-            </li>
+            {user ? 
+                (
+                    <li>
+                        <button onClick={onLogout} className='btn'>
+                            <FaSignOutAlt/> Logout
+                        </button>
+                    </li>
+                ) 
+                : (
+                    <>
+                        <li>
+                            <Link to='/login'>
+                                <FaSignInAlt /> Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/register'>
+                                <FaUser /> Register
+                            </Link>
+                        </li>
+                    </>
+                )
+            }
+            
         </ul>
     </header>
   )
