e8a6779ef59933809d39090cbc6e620d5f0a601f
Paula Oliveira <oliveirapaulaa@gmail.com>
2024-04-11 17:56:30 -0300

    Add display notes to ticket page

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'api/tickets/'
+const API_URL = '/api/tickets/'
 
 // Get Ticket Notes
 const getNotes = async (ticketId, token) => {
--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,14 +1,18 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice'
+import { getTicket, closeTicket } from '../features/tickets/ticketSlice'
+import { getNotes, reset as notesReset } from '../features/notes/noteSlice'
 import { useParams, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import BackButton from '../components/BackButton'
 import Spinner from '../components/Spinner'
-import ticketService from '../features/tickets/ticketService'
+import NoteItem from '../components/NoteItem'
 
 export default function Ticket() {
   const { ticket, isLoading, isSuccess, isError, message } = useSelector((state) => state.tickets)
+  const { notes, isLoading: notesIsLoading } = useSelector((state) => state.notes)
+  
+  
   const params = useParams()
   const navigate = useNavigate()
   const dispatch = useDispatch()
@@ -20,6 +24,8 @@ export default function Ticket() {
     }
 
     dispatch(getTicket(ticketId))
+    dispatch(getNotes(ticketId))
+
     // eslint-disable-next-line
   }, [isError, message, ticketId])
 
@@ -29,7 +35,7 @@ export default function Ticket() {
     navigate('/tickets')
   }
 
-  if (isLoading) {
+  if (isLoading || notesIsLoading) {
     return <Spinner/>
   }
 
@@ -52,7 +58,12 @@ export default function Ticket() {
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
+        <h2>Notes</h2>
       </header>
+
+      {notes.map((note) => (
+        <NoteItem key={note._id} note={note} />
+      ))}
 
       {ticket.status !== 'closed' && (
         <button onClick={onTicketClose} className='btn btn-block btn-danger'>Close Ticket</button>
